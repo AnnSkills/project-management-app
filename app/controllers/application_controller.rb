55fda26200c24387b12afecd87d930d990f1b9d1
Anna <khomiakova25@gmail.com>
2022-10-18 12:49:25 +0300

    fix: account and user connectiontenant config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
-
+  set_current_tenant_by_subdomain(:account, :subdomain)
   # before_action :authenticate_user!
   # set_current_tenant_by_subdomain_or_domain(:account, :subdomain, :domain)
 
   #
-  set_current_tenant_through_filter
-  before_action :set_current_account
-
-  def set_current_account
-    return unless current_user.present?
-    current_account = current_user.account
-    ActsAsTenant.current_tenant = current_account
-  end
+  #  set_current_tenant_through_filter
+  # before_action :set_current_account
+  #
+  # def set_current_account
+  #   return unless current_user.present?
+  #   current_account = current_user.account
+  #   ActsAsTenant.current_tenant = current_account
+  # end
   #
   # *******2
   # before_action :configure_permitted_parameters, if: :devise_controller?
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
          :confirmable
   has_one :account
   before_validation :set_account
-
+  before_create :set_account
+  #acts_as_tenant(:account)
   def set_account
     self.build_account
   end
